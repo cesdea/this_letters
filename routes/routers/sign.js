c112870f949ce34fca626316a8e943e378806b99
cesdea <aprkvnem@gmail.com>
2022-08-03 23:09:15 +0900

    create login,signup

---
+++ routes/routers/sign.js
@@ -0,0 +1,40 @@
+const express =require('express');
+const router = express.Router();
+const { Op } = require("sequelize");
+const jwt=require('jsonwebtoken');
+const models = require("../../models/index");
+
+router.post("/login",async (req,res)=>{
+    const {email,password}=req.body;
+    const user=await models.Users.findAll({
+        where:{email}
+    })
+    if(user.length==0){
+        return res.status(400).send("WRONG_EMAIL")
+    }
+    if(user.dataValues.password!=password){
+        return res.status(400).send("WRONG_PASSWORD")
+    }
+    const token=jwt.sign({ email: email }, "thisletter")
+        res.send({
+            token:token,
+    });
+    return res.status(200).send("SUCCESS_LOGIN")
+})
+
+router.post("/singup",async (req,res)=>{
+    const
+    {email,
+    nickname,
+    password,
+    confirmPassword
+    } = req.body
+    const signup=await models.Users.create({
+        email,
+        nickname,
+        password
+    })
+    res.status(201).send("가입성공");   
+})
+
+module.exports = router;
